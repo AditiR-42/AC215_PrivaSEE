982853849671dc974a9785c89c6f901a039a1049
shiradaronson <138694676+shiradaronson@users.noreply.github.com>
2024-12-03 10:04:23 -0500

    frontend container updates

--- src/frontend-react/src/app/page.jsx
+++ src/frontend-react/src/app/page.jsx
@@ -1,8 +1,13 @@
 import Hero from '@/components/home/Hero';
-import About from '@/components/home/About';
-import Podcasts from '@/components/home/Podcasts';
-import Newsletters from '@/components/home/Newsletters';
-import WhatIs from '@/components/home/WhatIs';
+// Ensure unused imports are removed or commented out if not needed
+// import About from '@/components/home/About';
+// import Podcasts from '@/components/home/Podcasts';
+// import Newsletters from '@/components/home/Newsletters';
+// import WhatIs from '@/components/home/WhatIs';
+
+// Import required CSS for react-pdf
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 export default function Home() {
     return (
@@ -11,5 +16,5 @@ export default function Home() {
             {/* <WhatIs></WhatIs>
             <About /> */}
         </>
-    )
+    );
 }
