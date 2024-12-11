5dc2d5ce3dc960582df63b671fad476e049722a8
yeabsira mohammed <yeabsiramohammed@college.harvard.edu>
2024-12-11 13:01:42 -0500

    changes

--- src/api_service/api/tests/test_integration.py
+++ src/api_service/api/tests/test_integration.py
@@ -1,6 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
-from .api.service import app
+import os
+print("this is path", os.getcwd())
+from api.service import app
 
 @pytest.fixture(scope="session")
 def test_client():
