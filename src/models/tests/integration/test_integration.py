e6b5598c9af5f88f73ff354089908a3e6d021fd8
yeabsira mohammed <yeabsiramohammed@college.harvard.edu>
2024-12-11 10:43:53 -0500

    test

--- src/models/tests/integration/test_integration.py
+++ src/models/tests/integration/test_integration.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from src.api_service.api.service import app
+from api_service.api.service import app
 
 @pytest.fixture(scope="session")
 def test_client():
