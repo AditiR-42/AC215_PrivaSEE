8fcf9d6609758e76d915b90f3347f39fca194e0d
yeabsira mohammed <yeabsiramohammed@college.harvard.edu>
2024-12-10 22:30:10 -0500

    update frontend and backend version and header color /deploy-app

--- src/frontend-react/src/app/about/page.jsx
+++ src/frontend-react/src/app/about/page.jsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
           <section className={styles.hero}>
               <div className={styles.heroContent}>
                   <h1>About Privasee 🔍</h1>
-                  <p>PrivaSEE aims to bridge knowledge gaps around data privacy by reviewing terms and conditions agreements and informing you about the aspects of the privacy you cede by using a certain app or website. PrivaSEE allows you to understand the implications to your data privacy, and compare options in a way that aligns with your personal privacy priorities. </p>
+                  <p>PrivaSEE aims to bridge knowledge gaps around data privacy by reviewing terms and conditions agreements and informing you about the aspects of the privacy you cede by using a certain app or website. PrivaSEE allows you to understand the implications to your data privacy, and compare options in a way that aligns with your personal privacy priorities! </p>
                   <br/>
                   <p>Our app uses data from ToS;DR to provide you with up-to-date privacy grades of any app you upload terms and conditions for. Our app will also give you an app recommendation based on the desired privacy priorities and app qualities you input. </p>
               </div>
